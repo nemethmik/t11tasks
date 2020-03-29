a31e7369cf7c2ee36b116358eecdc168e773a6bf
Miklos Nemeth <nemethmiklos1960@gmail.com>
2020-03-30 00:02:46 +0200

    Test script updated and local storage tests added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import {Game} from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('game start text search', () => {
+  const { getByText } = render(<Game />);
+  const linkElement = getByText(/game start/i);
   expect(linkElement).toBeInTheDocument();
 });
+test("Local Storage Test 1",()=>{
+  localStorage.setItem("Database","SBODemoUS")
+  const dbName = localStorage.getItem("Database")
+  console.log("dbName",dbName)
+  expect(dbName).toEqual("SBODemoUS")
+})
